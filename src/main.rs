0cef5bdeb5527e077b792d68dae51d845deb4f6c
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-16 17:15:08 -0400

    Implement initial working file read function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::fs;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let path = "./languages-on-github.yml";
+    let contents = read_file(path);
+    println!("Contents: {}", contents);
+}
+
+fn read_file(path: &str) -> String {
+    let contents = fs::read_to_string(path)
+        .expect("There was a problem reading the file");
+    contents
 }
