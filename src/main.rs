e95345f5f74b686bfd66856c4830d826a2aa0a38
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-16 17:39:42 -0400

    Create construct_blocks function

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,23 @@ fn main() {
     let path = "./languages-on-github.yml";
     let contents = read_file(path);
     let lines = split_into_lines(contents);
+    let blocks = construct_blocks(lines);
+}
+
+fn construct_blocks(lines: Vec<String>) -> Vec<String> {
+    let mut block = String::new();
+    let mut blocks = Vec::<String>::new();
+    for line in lines {
+        if line.chars().nth(0) != Some(' ') {
+            blocks.push(block);
+            block = String::from("");
+            block += &line;
+        }
+        else {
+            block += &line;
+        }
+    }
+    blocks
 }
 
 fn split_into_lines(file: String) -> Vec<String> {
