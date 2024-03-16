931ed653495773354f63c8fd6e09b5be98e51986
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-16 17:32:50 -0400

    Complete split_into_lines function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 use std::fs;
-use std::env;
 
 fn main() {
     let path = "./languages-on-github.yml";
     let contents = read_file(path);
-    println!("Contents: {}", contents);
+    let lines = split_into_lines(contents);
+}
+
+fn split_into_lines(file: String) -> Vec<String> {
+    let contents: Vec<&str> = file.split("\n").collect();
+    let language_contents = contents
+        .into_iter()
+        .filter(|line| line.chars().nth(0) != Some('#'))
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>();
+    language_contents
 }
 
 fn read_file(path: &str) -> String {
     let contents = fs::read_to_string(path);
     match contents {
         Ok(lines) => lines,
-        Err(_) => String::new(),
+        Err(_)    => String::new(),
     }
 }
