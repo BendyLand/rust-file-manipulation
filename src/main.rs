a49f3c5ffc9514ddfd4ff35321d5804fd465a72a
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-16 20:48:57 -0400

    Refactor chained method calls inside blocks

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,28 @@ fn main() {
     let contents = read_file(path);
     let lines = split_into_lines(contents);
     let blocks = construct_blocks(lines);
-    let formatted_blocks = blocks
-        .into_iter()
-        .map(|block| format_block(&block))
-        .collect::<Vec<String>>();
+    let formatted_blocks = {
+        blocks
+            .into_iter()
+            .map(|block| format_block(&block))
+            .collect::<Vec<String>>()
+    };
     println!("{}", formatted_blocks[0]);
 }
 
 fn format_block(block: &String) -> String {
-    let mut tokens = block
-        .split_whitespace()
-        .collect::<Vec<&str>>();
+    let mut tokens = {
+        block
+            .split_whitespace()
+            .collect::<Vec<&str>>()
+    };
     let language = extract_language(&mut tokens);
     let formatted_details = format_details(&tokens);
     format!("{}{}", language, formatted_details)
 }
 
 fn extract_language(tokens: &mut Vec<&str>) -> String {
-    let mut result = String::new();
+    let result: String;
     if tokens[0].contains(':') {
         result = tokens[0].to_string();
         *tokens = tokens[1..].to_vec();
@@ -71,11 +75,13 @@ fn construct_blocks(lines: Vec<String>) -> Vec<String> {
 
 fn split_into_lines(file: String) -> Vec<String> {
     let contents: Vec<&str> = file.split("\n").collect();
-    let language_contents = contents
-        .into_iter()
-        .filter(|line| line.chars().nth(0) != Some('#'))
-        .map(|x| x.to_string())
-        .collect::<Vec<String>>();
+    let language_contents = {
+        contents
+            .into_iter()
+            .filter(|line| line.chars().nth(0) != Some('#'))
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+    };
     language_contents
 }
 
